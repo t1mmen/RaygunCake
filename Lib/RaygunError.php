3cdd3504823db01ed2b9a31d3b499da16a15ef8e
Timm Stokke <timm@stokke.me>
2013-12-13 23:39:06 +0100

    Spell out Raygun4php's params. Async now turned on by default, debugMode is off.

--- Lib/RaygunError.php
+++ Lib/RaygunError.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Send along errors and exceptions to Raygun.io
- * 
+ *
  * @link        http://raygun.ui
  * @link        https://github.com/t1mmen/RaygunCake
  * @author 	Timm Stokke <timm@stokke.me>
@@ -11,22 +11,32 @@ App::import('Vendor', 'Raygun4php', array('file' => 'Raygun4php' . DS . 'RaygunC
 
 class RaygunError extends ErrorHandler {
 
+	// Overload Errors:
 	public static function handleError($code, $description, $file = null, $line = null, $context = null) {
+
+		// Raygun settings
+		$useAsyncSending = true;
+		$debugMode = false;
 
 		// Call Raygun
 		$apiKey  = Configure::read('RaygunCake.apiKey'); // This is required
-		$client = new \Raygun4php\RaygunClient($apiKey, false, true);
+		$client = new \Raygun4php\RaygunClient($apiKey, $useAsyncSending, $debugMode);
 		$client->SendError($code, $description, $file, $line);
 
 		// Fall back to cake
 		return parent::handleError($code, $description, $file, $line, $context);
 	}
 
+	// Overload Exceptions:
 	public static function handleException(Exception $exception) {
+
+		// Raygun settings
+		$useAsyncSending = true;
+		$debugMode = false;
 
 		// Call Raygun
 		$apiKey  = Configure::read('RaygunCake.apiKey'); // This is required
-		$client = new \Raygun4php\RaygunClient($apiKey, false, true);
+		$client = new \Raygun4php\RaygunClient($apiKey, $useAsyncSending, $debugMode);
 		$client->SendException($exception);
 
 		// Fall back to Cake..
