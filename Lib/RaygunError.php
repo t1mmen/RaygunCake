5a7fe1766e266bf0c7dd29c4254579a42ec7c98c
Dan Deming <drlongghost@yahoo.com>
2015-06-09 13:32:57 -0400

    Add ability to call Raygun directly
    Two new public methods were added to allow Raygun to be called
    independently of Cake's error handling
    Added class variables for debug and async settings

--- Lib/RaygunError.php
+++ Lib/RaygunError.php
@@ -10,39 +10,40 @@
 App::import('Vendor', 'Raygun4php', array('file' => 'Raygun4php' . DS . 'RaygunClient.php'));
 
 class RaygunError extends ErrorHandler {
+	public static $useAsyncSending = true;
 
-	// Overload Errors:
-	public static function handleError($code, $description, $file = null, $line = null, $context = null) {
+	public static $debugMode = false;
 
-		// Raygun settings
-		$useAsyncSending = true;
-		$debugMode = false;
-
+	// Call Raygun without calling handleError()
+	public static function handleErrorRaygun($code, $description, $file = null, $line = null) {
 		// Call Raygun
-		$apiKey  = Configure::read('RaygunCake.apiKey'); // This is required
-		$client = new \Raygun4php\RaygunClient($apiKey, $useAsyncSending, $debugMode);
+		$apiKey = Configure::read('RaygunCake.apiKey'); // This is required
+		$client = new \Raygun4php\RaygunClient($apiKey, self::$useAsyncSending, self::$debugMode);
 		$client->SendError($code, $description, $file, $line);
+	}
+
+	// Call Raygun without calling handleException
+	public static function handleExceptionRaygun(Exception $exception) {
+		// Call Raygun
+		$apiKey = Configure::read('RaygunCake.apiKey'); // This is required
+		$client = new \Raygun4php\RaygunClient($apiKey, self::$useAsyncSending, self::$debugMode);
+		$client->SendException($exception);
+	}
+
+	// Overload Errors: Call Raygun AND handleError()
+	public static function handleError($code, $description, $file = null, $line = null, $context = null) {
+		self::handleErrorRaygun($code, $description, $file, $line);
 
 		// Fall back to cake
 		return parent::handleError($code, $description, $file, $line, $context);
 	}
 
-	// Overload Exceptions:
+	// Overload Exceptions: Call Raygun AND handleException()
 	public static function handleException(Exception $exception) {
-
-		// Raygun settings
-		$useAsyncSending = true;
-		$debugMode = false;
-
-		// Call Raygun
-		$apiKey  = Configure::read('RaygunCake.apiKey'); // This is required
-		$client = new \Raygun4php\RaygunClient($apiKey, $useAsyncSending, $debugMode);
-		$client->SendException($exception);
+		self::handleExceptionRaygun($exception);
 
 		// Fall back to Cake..
 		return parent::handleException($exception);
 	}
-
-
 
 }
